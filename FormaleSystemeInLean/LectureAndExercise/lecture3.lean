a830a280676017ebbe27a57591779713fcaf9fea
anna-dolma <anna_dolma.hornschild@mailbox.tu-dresden.de>
2025-12-06 15:02:57 +0100

    first direction of totalDFA_eq_DFA

--- FormaleSystemeInLean/LectureAndExercise/lecture3.lean
+++ FormaleSystemeInLean/LectureAndExercise/lecture3.lean
@@ -15,7 +15,7 @@ structure NFA (Q : Type u) (Sigma : Type v) [Fintype Q] [Fintype Sigma] where
   Q0 : List Q
   F : List Q
 
-variable {Q : Type u} {Sigma : Type v} [Fintype Q] [Fintype Sigma]
+variable {Q : Type u} {Sigma : Type v} [Fintype Q] [Fintype Sigma] [Fintype (Option Q)]
 
 def DFA.δ_word (dfa : DFA Q Sigma) (q : Q) : (Word Sigma) -> Option Q
 | .nil => .some q
@@ -49,17 +49,18 @@ def NFA.Language (nfa : NFA Q Sigma) : Language Sigma :=
 -- Äquivalenz von NFAs und DFAs mittels Potenzmengenkonstruktion
 
 def DFA.to_totalDFA (M : DFA Q Sigma) [Fintype (Option Q)] : TotalDFA (Option Q) Sigma where
-  δ := fun q a =>
+  /-δ := fun q a =>
     match q with
       | none => none
       | some q =>
         match M.δ q a with
         | none   => none
-        | some q' => some q'
+        | some q' => some q'-/
+  δ := fun q a => q.bind (M.δ · a)
   q0 := .some M.q0
   F := M.F.map fun q => some q
 
-theorem a (M : DFA Q Sigma) (q : Q) [Fintype (Option Q)] : q ∈ M.F ↔ some q ∈ M.to_totalDFA.F := by
+theorem final_iff_final (M : DFA Q Sigma) (q : Q) : q ∈ M.F ↔ some q ∈ M.to_totalDFA.F := by
   constructor
   . intro q_mem
     unfold DFA.to_totalDFA
@@ -68,21 +69,67 @@ theorem a (M : DFA Q Sigma) (q : Q) [Fintype (Option Q)] : q ∈ M.F ↔ some q 
     unfold DFA.to_totalDFA at q_mem
     grind
 
-theorem totalDFA_eq_DFA (M : DFA Q Sigma) [Fintype (Option Q)] : M.Language = (M.to_totalDFA).Language := by
+theorem δ_eq_δ (M : DFA Q Sigma) (q : Q) (a : Sigma) : M.δ q a = M.to_totalDFA.δ q a := by
+  unfold DFA.to_totalDFA
+  simp
+
+theorem δ_none_eq_none (M : DFA Q Sigma) (a : Sigma) : M.to_totalDFA.δ none a = none := by
+  trivial -- to do: ohne trivial???
+
+theorem garbage_state (M : DFA Q Sigma) (w : Word Sigma) : M.to_totalDFA.δ_word none w = none := by
+  induction w with
+  | nil =>
+    trivial
+  | cons a v ih =>
+    unfold TotalDFA.δ_word
+    rw [δ_none_eq_none]
+    exact ih
+
+theorem δ_word_eq_δ_word (M : DFA Q Sigma) (q : Q) (w : Word Sigma) : M.δ_word q w = M.to_totalDFA.δ_word q w := by
+  induction w generalizing q with
+  | nil =>
+    trivial
+  | cons a v ih =>
+    rw [DFA.δ_word, TotalDFA.δ_word]
+    simp only [δ_eq_δ]
+    cases hd : M.to_totalDFA.δ (some q) a with
+    | none =>
+      simp only [Option.bind]
+      unfold TotalDFA.δ_word
+      cases hv : v with
+      | nil =>
+        simp
+      | cons a' v' =>
+        simp only [hv]
+        rw [δ_none_eq_none]
+        rw [garbage_state]
+    | some q' =>
+      apply ih
+
+theorem totalDFA_eq_DFA (M : DFA Q Sigma) : M.Language = (M.to_totalDFA).Language := by
   apply Set.ext
   intro w
   constructor
   . intro w_mem
     rcases w_mem with ⟨qf, w_acc, qf_mem⟩
-    induction w generalizing qf with
+    rw [δ_word_eq_δ_word] at w_acc
+    rw [final_iff_final] at qf_mem
+    unfold TotalDFA.Language
+    have aux : M.to_totalDFA.q0 = some M.q0 := by simp only [DFA.to_totalDFA]
+    simp only [Membership.mem, aux]
+    rw [w_acc]
+    exact qf_mem
+  . intro w_mem
+    cases w with
     | nil =>
-      simp only [DFA.δ_word] at w_acc
-      unfold TotalDFA.Language TotalDFA.δ_word
+      exists M.q0
+      rw [final_iff_final, δ_word_eq_δ_word]
+      simp only [TotalDFA.δ_word]
+      trivial
+    | cons a v =>
+      unfold DFA.Language
       simp only [Membership.mem]
-      simp at w_acc
-      rw [a] at qf_mem
-      rw [DFA.to_totalDFA]
+      rw [δ_word_eq_δ_word]
+      simp only [TotalDFA.δ_word]
 
       sorry
-    | cons a v ih => sorry
-  . sorry
